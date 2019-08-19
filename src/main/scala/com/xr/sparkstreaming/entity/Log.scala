2c5d3046a4b0e19dd34565f7963bc4c6b2e51582
rising001 <xurong_china@163.com>
2019-08-20 04:35:51 +0800

    日志样例类

---
+++ src/main/scala/com/xr/sparkstreaming/entity/Log.scala
@@ -0,0 +1,11 @@
+package com.xr.sparkstreaming.entity
+
+import java.time.LocalDateTime
+
+/**
+  * User:xr
+  * Date:2019/8/20 4:32
+  * Description:日志样例类
+  **/
+
+case class Log(time: LocalDateTime, level: String)
